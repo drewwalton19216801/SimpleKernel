a8750ddc8a6098d247a8311cddfdae65389d8d7c
Drew Walton <drewwalton19216801@gmail.com>
2013-08-28 17:04:48 -0600

    It boots

---
+++ src/main.c
@@ -0,0 +1,7 @@
+// main.c -- Defines the C-code kernel entry point, calls initialisation routines.
+
+int main(struct multiboot *mboot_ptr)
+{
+    // All our initialisation calls will go in here.
+    return 0xDEADBABA;
+}
